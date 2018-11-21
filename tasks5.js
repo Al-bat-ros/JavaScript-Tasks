5df894a619ef66f49c8478c8c2a7ce0a091df22a
Al-bat-ros <al-bat-ros@mail.ru>
2018-11-21 12:10:03 +0500

    Add array

---
+++ tasks5.js
@@ -0,0 +1,21 @@
+// Создание массива
+var arr = [1,2,3,4,5,6];
+
+var arr1 = [];
+arr1[0] = 1;
+arr1[1] = 2;
+arr1[2] = 3;
+arr1[3] = 4;
+arr1[4] = 5;
+arr1[5] = 6;
+
+var arr2 = [];
+arr2.push(1);
+arr2.push(2);
+arr2.push(3);
+arr2.push(4);
+arr2.push(5);
+arr2.push(6);
+document.write(arr+'<br>');
+document.write(arr1+'<br>');
+document.write(arr2+'<br>');

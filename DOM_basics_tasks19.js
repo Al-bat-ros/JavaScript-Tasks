74b229f21c5136c094fb8692b02187896fc6f5b6
Al-bat-ros <al-bat-ros@mail.ru>
2018-12-19 00:11:55 +0500

    Blocking input fields

--- DOM_basics_tasks19.js
+++ DOM_basics_tasks19.js
@@ -1,5 +1,9 @@
-
+//
 //Задача 4 . Квадрат содержимого инпута
+//
+//  <button onclick="buttonClick()">Нажми на меня</button>
+//  <input type="text" id="input" placeholder="Введите число!"
+//
 /*
 function buttonClick() {
 	var input = document.getElementById('input');
@@ -8,9 +12,13 @@ function buttonClick() {
 	alert(square);
 }
 */
-
+//-------------------------------------------------------
 
 // Задача 5 . Обмен содержимым между инпутами
+//
+//   <button onclick="buttonClick()">Нажми на меня</button>
+//   <input type="text" id="input1" value="!!!"><input type="text" id="input2" value="???"
+//
 /*
 function buttonClick() {
 	var input1 = document.getElementById('input1');
@@ -21,18 +29,27 @@ function buttonClick() {
 	input2.value = input1Value;
 }
 */
+//-------------------------------------------------------
 
 
 // Задача 6. По нажатию на кнопку меняется ее текст
 // Решим задачу через this:
+//
+//   <input type="button" onclick="buttonClick(this)" value="Нажми на меня">
+//
 /*
 function buttonClick(elem) {
 	elem.value = 'Новый текст кнопки';
 }
 */
+//--------------------------------------------------------
 
 
 // Задача 7. Работа с CSS
+//
+//   <button onclick="buttonClick()">Нажми на меня</button>
+//   <input type="text" id="input" value="Какой-то текст!">
+//
 /*
 function buttonClick() {
 
@@ -41,3 +58,26 @@ function buttonClick() {
 
 }
 */
+//--------------------------------------------------------
+
+// Задача 8. Блокирование поле ввода
+//
+//<button onclick="button1Click()">Заблокировать</button>
+//<button onclick="button2Click()">Отблокировать</button>
+//<input type="text" id="input" value="Какой-то текст!">
+//
+//
+// заблокирует 
+/*
+function buttonClick1() {
+	var input = document.getElementById('input');
+	input.disabled = true;
+
+}
+// разблокирует
+function buttonClick2() {
+	var input = document.getElementById('input');
+	input.disabled = false;
+}
+*/
+//--------------------------------------------------------

5e39f388858e7404f2f5eff86973f27465893017
Al-bat-ros <al-bat-ros@mail.ru>
2018-12-18 23:58:14 +0500

    working with CSS

--- DOM_basics_tasks19.js
+++ DOM_basics_tasks19.js
@@ -1,7 +1,43 @@
 
-function buttonClick(){
+//Задача 4 . Квадрат содержимого инпута
+/*
+function buttonClick() {
 	var input = document.getElementById('input');
 	var number = Number(input.value);
-	var square = number * number;
+	var square = number*number;
 	alert(square);
 }
+*/
+
+
+// Задача 5 . Обмен содержимым между инпутами
+/*
+function buttonClick() {
+	var input1 = document.getElementById('input1');
+	var input2 = document.getElementById('input2');
+	var input1Value = input1.value;
+	var input2Value = input2.value;
+	input1.value = input2Value;
+	input2.value = input1Value;
+}
+*/
+
+
+// Задача 6. По нажатию на кнопку меняется ее текст
+// Решим задачу через this:
+/*
+function buttonClick(elem) {
+	elem.value = 'Новый текст кнопки';
+}
+*/
+
+
+// Задача 7. Работа с CSS
+/*
+function buttonClick() {
+
+	var input = document.getElementById('input');
+	input.style.color = 'red';
+
+}
+*/

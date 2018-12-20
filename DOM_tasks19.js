1855a94375050e03dc8d0ba76a60990364c43cea
Al-bat-ros <al-bat-ros@mail.ru>
2018-12-20 23:56:13 +0500

    Work with CSS 3

--- DOM_tasks19.js
+++ DOM_tasks19.js
@@ -1,4 +1,8 @@
-
+// задача
+// <input type="text" id="input" value="Ку-ку"><button onclick="hide()">Нажми на меня!</button>
+// <button onclick='show()'>Нажми потом на меня</button>
+//
+/*
 function hide(){
    var elem = document.getElementById('input');
    elem.style.display = 'none';
@@ -6,4 +10,20 @@ function hide(){
 function show(){
 	elem = document.getElementById('input');
 	elem.style.display = 'inline-block';
+}
+*/
+//---------------------------------------------------------
+//
+// задача
+//
+//
+//
+function hide() {
+	var elem = document.getElementById('input');
+	elem.value = 'Ой я поменяла свой текст и css';
+	elem.style.width = '300px';
+	elem.style.height = '50px';
+	elem.style.color = 'red';
+	elem.style.borderRadius = '10px';
+
 }

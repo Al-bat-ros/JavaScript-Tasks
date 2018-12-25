e92de2a11b907201f274efc78eca0833ec77bef6
Al-bat-ros <al-bat-ros@mail.ru>
2018-12-26 00:07:17 +0500

    document.forms.name_form

--- tasks20.js
+++ tasks20.js
@@ -1,7 +1,5 @@
 
 function func() {
-	var forms = document.forms;
-	for (var i = 0; i < forms.length; i++){
-		forms[i].innerHTML = '!' ;
-	}
+	document.forms.form1.innerHTML = '!';
+	document.forms['form2'].innerHTML = '?';
 }

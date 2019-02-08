c1a52cdbd6088ddc6d27b3b99c5bfa885d9e504f
Al-bat-ros <al-bat-ros@mail.ru>
2019-02-09 01:54:29 +0500

    date = new Date

--- tasks21_Date.js
+++ tasks21_Date.js
@@ -1,4 +1,14 @@
+//
+/*
 var date = new Date();
 var day = date.getDay();
 var days = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
 alert(days[day]);
+*/
+/*
+var date = new Date();
+var day = date.getDay();
+var month = date.getMonth()+1;
+var year = date.getFullYear();
+document.write(day+'-'+month+'-'+year);
+*/

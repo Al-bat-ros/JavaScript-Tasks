1c843e5de17752ef1c8eec5aae05ed853d8b2043
Al-bat-ros <al-bat-ros@mail.ru>
2019-02-25 03:35:00 +0500

    date = new Date2

--- tasks21_Date.js
+++ tasks21_Date.js
@@ -13,9 +13,56 @@ var year = date.getFullYear();
 document.write(day+'-'+month+'-'+year);
 */
 //
+/*
 var months = [
 	'янв', 'фев', 'мар', 'апр', 'май', 'июн',
 	'июл', 'авг', 'сен', 'окт', 'ноя', 'дек'
 ];
 var date = new Date();
 alert(months[date.getMonth()]);
+*/
+
+/*
+function go(num){
+	var s = '';
+ while (num > 0){
+		s = String(num%2)+s;
+		num = num/2;
+	}
+    return s
+}
+ console.log(go(9));
+ */
+ function go(){
+ var date = new Date();
+ var seconds = date.getSeconds();
+ var minutes = date.getMinutes();
+ var hours = date.getHours();
+ var day = date.getDay();
+ var months = date.getDay()+1;
+ var year = date.getFullYear();
+
+ if (hours <= 9){
+ 	var hours = '0' + hours;
+ }
+
+ if (minutes <= 9){
+ 	var minutes = '0' + minutes;  
+ }
+
+ if (seconds <= 9){
+ 	var seconds = '0' + seconds;
+ }
+ if (day <= 9){
+ 	var day = '0' + day;
+ }
+ if (months <= 9){
+    var months = '0' + months;
+ }
+
+ var time = hours+':'+minutes+':'+seconds+' '+day+'.'+months+'.'+year;
+document.write(time);
+}
+window.setInterval(go, 1000);
+
+ 
